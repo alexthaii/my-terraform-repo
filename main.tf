0740dda8edea4047265a1828e440781ed693d00b
Alex T <axt.421@gmail.com>
2026-07-09 16:24:24 -0400

    chore: remove ec2

--- main.tf
+++ main.tf
@@ -92,15 +92,3 @@ resource "aws_s3_bucket" "orbit_storage4" {
     environment = "demo"
   }
 }
-
-resource "aws_instance" "demo" {
-  ami    = "ami-0c02fb55956c7d316"
-  instance_type = "t3.micro"
-  tags = {
-    name        = "Orbit Labs Storage"
-    managedBy   = "Spacelift"
-    mission     = "First Launch"
-    project     = "Orbit-labs"
-    environment = "demo"
-  }
-}
