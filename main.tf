9e30d7dd48b4eabfd2a290ac82397123ed5d6e44
Alex Thai <alexthai@Alexs-MacBook-Air.local>
2026-07-10 12:47:49 -0400

    feat: reset

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "random_pet" "orbit_mascot" {
 }
 
 resource "aws_s3_bucket" "blast_radius_demo" {
-  count = 150
+  count = 1
 
   bucket_prefix = "blast-radius-demo-"
 
@@ -59,14 +59,14 @@ resource "aws_s3_bucket" "blast_radius_demo" {
   }
 }
 
-resource "aws_instance" "demo" {
-  ami    = "ami-0c02fb55956c7d316"
-  instance_type = "t3.micro"
-  tags = {
-    name        = "Orbit Labs Storage"
-    managedBy   = "Spacelift"
-    mission     = "First Launch"
-    project     = "Orbit-labs"
-    environment = "demo"
-  }
-}
+# resource "aws_instance" "demo" {
+#   ami    = "ami-0c02fb55956c7d316"
+#   instance_type = "t3.micro"
+#   tags = {
+#     name        = "Orbit Labs Storage"
+#     managedBy   = "Spacelift"
+#     mission     = "First Launch"
+#     project     = "Orbit-labs"
+#     environment = "demo"
+#   }
+# }
