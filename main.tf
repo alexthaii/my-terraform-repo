ad6d7e2bdf065749948173d2c353f89555ae0216
Alex Thai <alexthai@Alexs-MacBook-Air.local>
2026-07-10 09:42:21 -0400

    feat: add ec2 instance

--- main.tf
+++ main.tf
@@ -59,14 +59,14 @@ resource "aws_s3_bucket" "blast_radius_demo" {
   }
 }
 
-# resource "aws_instance" "demo" {
-#   ami    = "ami-0c02fb55956c7d316"
-#   instance_type = "t3.micro"
-#   tags = {
-#     name        = "Orbit Labs Storage"
-#     managedBy   = "Spacelift"
-#     mission     = "First Launch"
-#     project     = "Orbit-labs"
-#     environment = "demo"
-#   }
-# }
+resource "aws_instance" "demo" {
+  ami    = "ami-0c02fb55956c7d316"
+  instance_type = "t3.micro"
+  tags = {
+    name        = "Orbit Labs Storage"
+    managedBy   = "Spacelift"
+    mission     = "First Launch"
+    project     = "Orbit-labs"
+    environment = "demo"
+  }
+}
